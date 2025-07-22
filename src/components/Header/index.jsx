f2462786b007ed5ffc8355c80ba388a3db2aad63
Uriel Arana <udan1718@gmail.com>
2025-07-21 19:51:05 -0600

    Feat: Add react-lazy-load-image-component for optimized image loading, update styles and animations in Home, Header, Footer, and Sidebar components. Init StackButton Component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Header() {
   return (
-    <div className="flex justify-between items-center bg-aw-dark-30 text-white p-4">
+    <div className="flex justify-between items-center bg-pf-dark-30 text-white p-4">
       <div>Portfolio</div>
       <Navbar />
     </div>
--- src/components/Sidebar/NavItems/index.jsx
+++ src/components/Sidebar/NavItems/index.jsx
@@ -14,8 +14,8 @@ export default function NavItems({
   return (
     <Link
       to={url}
-      className={`grid grid-cols-6 p-2 rounded-lg cursor-pointer hover:bg-aw-dark-20 transition-colors duration-200 w-full ${
-        active ? "bg-aw-bluegray" : ""
+      className={`grid grid-cols-6 p-2 rounded-lg cursor-pointer hover:bg-pf-dark-20 transition-colors duration-200 w-full ${
+        active ? "bg-pf-bluegray" : ""
       }`}
     >
       <div className={`flex items-center justify-center col-span-1`}>
